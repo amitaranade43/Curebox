84ad0a2741880751712f7bf452db06aba8876f86
Vaibhavi Patel <vakpatel@iu.edu>
2022-12-08 22:50:59 -0500

    doctor home page changes

--- project/doctor/doctorUtility.py
+++ project/doctor/doctorUtility.py
@@ -2,7 +2,7 @@ from flask import Blueprint, redirect,render_template,request, url_for
 from flask_login import login_required, current_user
 from project import db
 from project.common import commonUtility
-from project.models import Doctor, Patient
+from project.models import Patient, User , Booking
 
 doctorUtility = Blueprint('doctorUtility', __name__)
 
@@ -14,7 +14,9 @@ def index():
 @doctorUtility.route('/doctor/')
 @login_required
 def doctor():
-    return render_template('doctor/doctor.html', name = 'doctor')
+    query = "select pt.firstname,pt.lastname,b.date,b.start_time,b.end_time,b.patient_id from booking b join patient pt on b.patient_id = pt.id where b.status='OPEN' order by b.start_time;"
+    records = db.engine.execute(query)
+    return render_template('doctor/doctor.html', name=current_user.first_name ,appointments = records)
   
   
 @doctorUtility.route('/searchdoctor',methods=['POST'])
@@ -24,9 +26,40 @@ def searchdoctor():
     disease = request.form.get('disease')
     covid_care = request.form.get('covid')
     location = request.form.get('location')
-    query = commonUtility.getQuery(searchquery,disease,covid_care,location);
+    query = commonUtility.getQuery(searchquery,disease,covid_care,location)
     records = db.engine.execute(query)
     diseases = db.engine.execute("select name from disease;")
     locations = db.engine.execute("select distinct h.location from hospital h join doctor d on h.id = d.hospital_id order by 1")
     return render_template('patient/patient.html', name=current_user.first_name, doctors = records ,diseases = diseases, locations = locations)
 
+
+@doctorUtility.route('/completeAppointment/<string:patientId>',methods=['GET','POST'])
+@login_required
+def completeAppointment(patientId : str):
+    patient = Patient.query.filter_by(id = patientId).first()
+    return render_template('doctor/completeAppointment.html', patient=patient)
+
+
+@doctorUtility.route('/markAsCompleteAppointment/<string:patientId>',methods=['POST'])
+@login_required
+def markAsCompleteAppointment(patientId : str):
+
+    doctor = User.query.filter_by(email=current_user.email).first()
+    print('doctor id is ', doctor.id)
+    print('patient id is ', patientId)
+
+    update_booking = Booking.query.filter_by(patient_id = patientId , doctor_id = doctor.id).first()
+
+    update_booking.status = 'COMPLETE'
+    db.session.commit()
+
+    # updateQuery = "update booking set status='OPEN' where patient_id = "+ str(patientId) + "  and doctor_id = "+ str(doctor.id) +" ;"
+    # db.engine.execute(updateQuery)
+    query = "select pt.firstname,pt.lastname,b.date,b.start_time,b.end_time,b.patient_id from booking b join patient pt on b.patient_id = pt.id where b.status='OPEN' order by b.start_time;"
+    records = db.engine.execute(query)
+    return render_template('doctor/doctor.html', name=current_user.first_name ,appointments = records)
+  
+    
+
+    
+    
