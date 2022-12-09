9af0a0c791283203c64595ceb0ec93f1fe555f73
Akhila Eshapula <akesha@iu.edu>
2022-12-08 19:01:00 -0500

    akhila - insurance package changes

--- project/models/InsuranceProvider.py
+++ project/models/InsuranceProvider.py
@@ -2,8 +2,10 @@ from project import db
 
 class InsuranceProvider(db.Model):
     __tablename__ = 'InsuranceProvider'
-    package_name = db.Column(db.String(255),primary_key=True)
+    id = db.Column(db.Integer,primary_key=True)
+    package_name = db.Column(db.String(255))
     package_description = db.Column(db.String(255))
     insurance_duration = db.Column(db.Integer)
     age = db.Column(db.String(255))
     price = db.Column(db.Integer)
+
